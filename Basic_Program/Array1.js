c23f42052d5b7a8d495f8d03be1c04ab11e86d78
Ayan Samai <ayansamai14@gmail.com>
2025-11-23 20:20:27 +0530

    Add files via upload

---
+++ Basic_Program/Array1.js
@@ -0,0 +1,52 @@
+const arr=[1,2,3,4]
+// console.log(arr[2])
+
+// console.log(arr.includes(3)); 
+// console.log(arr.indexOf(3));
+
+// const newArr1=arr.slice(1,3)
+// console.log(newArr1)
+// const newArr=arr.splice(1,3)
+// console.log(newArr)
+
+// const Arr=[7,5,[6,8],[9,10]]
+// const newArr=arr.concat(Arr)
+// console.log(newArr)
+// console.log(...arr,...Arr)
+
+// const Array1=new Array()
+// Array1.push(1)
+// console.log(Array1)
+
+// for(let i=0; i<arr.length;i++){
+//     // console.log(arr[i])
+// }
+
+// //Function Defition:
+// function first(e){
+//     //Function
+//     //console.log("Ayan Samai")
+// }
+
+// //Function Declare
+// first()
+
+// const ArrowFunction=((e)=>{
+//     //console.log(e)
+// })
+// ArrowFunction(20)
+
+// const a=arr.forEach((e)=>{
+//     console.log(e)
+//     return e
+// })
+// console.log(a)
+
+// const b=arr.map((e)=>{
+//     return e
+// })
+// console.log(b)
+
+arr[3]=0
+console.log(arr)
+
